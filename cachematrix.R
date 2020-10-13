8273b9293280a77031a189bd3a9d876477e111a3
ACGG5361 <72767589+ACGG5361@users.noreply.github.com>
2020-10-13 13:01:23 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Makes a special matrix, from x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+## cacheSolve, makes the inverse of the matrix x.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m<- dim(x)
+## This is for prove that x isn't null.
+  if(!is.null(m)) {
+    I<- diag(1, dim(x))
+## Make an identity matrix
+    message("getting cached data")
+    n<- solve(x,I)
+##Doing the inverse of the matrix x, with the help of I
+    n
+##Printing the inverse of the matrix x.
+  }
+  }
